bbf38c2f3d51e889761f114008f5f5f19b4952d2
JBlaney93 <jamesblaney93@gmail.com>
2023-11-05 22:44:33 +0000

    made up some data for what a fighter user type could look like

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,17 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <h1 className="mb-4">Homepage</h1>
+      <h1 className="text-4xl mb-4">Dashboard</h1>
       <div className="flex justify-between items-center mb-4">
-        <Link href="users/create-a-fighter">Create a Fighter</Link>
-        <Link href="users/fighter/{id}">Profile</Link>
-        <Link href="users/view-all-fighters">View all Fighters</Link>
+        <Link href="users/create-a-fighter" className="text-2xl">
+          Create a Fighter
+        </Link>
+        <Link href="users/fighter/{id}" className="text-2xl">
+          Profile
+        </Link>
+        <Link href="users/view-all-fighters" className="text-2xl">
+          View all Fighters
+        </Link>
       </div>
     </>
   );
--- src/app/users/fighter/[id]/page.tsx
+++ src/app/users/fighter/[id]/page.tsx
@@ -1,5 +1,75 @@
 import Link from "next/link";
 
 export default function FighterProfilePage() {
-  return <h1>Fighter Profile</h1>;
+  type Fighter = {
+    firstName: string;
+    lastName: string;
+    nickName: string;
+    gender: string;
+    fightingWeight: number;
+    weightClass: string;
+    gym: string;
+    city: string;
+    country: string;
+    professional: boolean;
+    wins: number;
+    losses: number;
+  };
+
+  const fighter: Fighter = {
+    firstName: "James",
+    lastName: "Blaney",
+    nickName: "Big Boy",
+    gender: "male",
+    fightingWeight: 84,
+    weightClass: "Middle Weight",
+    gym: "MNBJJ",
+    city: "Glasgow",
+    country: "Scotland",
+    professional: false,
+    wins: 3,
+    losses: 0,
+  };
+
+  const professional = fighter.professional;
+  const fightingWeightInLbs = Math.round(fighter.fightingWeight * 2.20462);
+
+  return (
+    <>
+      <h1 className="text-4xl mb-4 border-b border-secondayColour max-w-sm">
+        Fighter Profile
+      </h1>
+      <div className="flex flex-col gap-4">
+        <h2 className="text-2xl">
+          {fighter.firstName} "{fighter.nickName}" {fighter.lastName}
+        </h2>
+        <div>
+          {!professional ? (
+            <section className="flex items-baseline gap-1">
+              <h2 className="text-2xl">
+                {fighter.wins} - {fighter.losses}
+              </h2>
+              <p>amature</p>
+            </section>
+          ) : (
+            <section className="flex items-baseline gap-1">
+              <h2 className="text-2xl">
+                {fighter.wins} - {fighter.losses}
+              </h2>
+              <p>professional</p>
+            </section>
+          )}
+          <h3 className="text-xl">{fighter.weightClass}</h3>
+          <h3 className="text-xl">
+            {fighter.fightingWeight}kgs ({fightingWeightInLbs}lbs)
+          </h3>
+        </div>
+
+        <div>
+          <h4>Training at {fighter.gym}</h4>
+          <h4>Fighting out of {fighter.city}</h4>
+        </div>
+      </div>
+    </>
+  );
 }
