874a6822f15e7ad47e325a15e0d617f11301dac9
JBlaney93 <jamesblaney93@gmail.com>
2023-11-07 18:53:42 +0000

    started making cards to display fighter info

--- src/app/users/view-all-fighters/page.tsx
+++ src/app/users/view-all-fighters/page.tsx
@@ -1,5 +1,91 @@
 import Link from "next/link";
+import { Fighter } from "../fighter/[id]/page";
 
 export default function FightersListPage() {
-  return <h1>Fighters List</h1>;
+  const fighter1: Fighter = {
+    id: 1,
+    firstName: "James",
+    lastName: "Blaney",
+    nickName: "Big Boy",
+    gender: "male",
+    fightingWeight: 84,
+    weightClass: "Middle Weight",
+    gym: "MNBJJ",
+    city: "Glasgow",
+    country: "Scotland",
+    professional: false,
+    wins: 3,
+    losses: 0,
+  };
+
+  const fighter2: Fighter = {
+    id: 2,
+    firstName: "Josh",
+    lastName: "Mackie",
+    nickName: "Blaster",
+    gender: "male",
+    fightingWeight: 84,
+    weightClass: "Middle Weight",
+    gym: "YSL Street Fighters",
+    city: "East Kilbride",
+    country: "Scotland",
+    professional: false,
+    wins: 17,
+    losses: 5,
+  };
+
+  const fighter3: Fighter = {
+    id: 3,
+    firstName: "Wully",
+    lastName: "McGoldrick",
+    nickName: "Handsome",
+    gender: "male",
+    fightingWeight: 84,
+    weightClass: "Middle Weight",
+    gym: "Aberdeen Combat Club",
+    city: "Aberdeen",
+    country: "Scotland",
+    professional: false,
+    wins: 2,
+    losses: 10,
+  };
+
+  const fighter4: Fighter = {
+    id: 4,
+    firstName: "Derek",
+    lastName: "Donaldson",
+    nickName: "Smelly",
+    gender: "male",
+    fightingWeight: 84,
+    weightClass: "Middle Weight",
+    gym: "East End Scrappers",
+    city: "Glasgow",
+    country: "Scotland",
+    professional: false,
+    wins: 27,
+    losses: 59,
+  };
+
+  const fighterList: Fighter[] = [fighter1, fighter2, fighter3, fighter4];
+
+  return (
+    <>
+      <h1 className="text-4xl mb-4">Fighters List</h1>
+      <div className="flex flex-col gap-2">
+        {fighterList.map((fighter) => {
+          return (
+            <div className="w-96 h-auto border-2 border-themeBlack rounded bg-mainBackground text-mainText p-4 flex flex-col">
+              <p>
+                {fighter.firstName} "{fighter.nickName}" {fighter.lastName}
+              </p>
+              <p>
+                {fighter.wins} - {fighter.losses}
+              </p>
+              <p>{fighter.weightClass}</p>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
 }
