846d95f6b35cf12d2e36084a39424a425c1ececc
unknown <joshuamackie10@googlemail.com>
2023-11-07 17:52:51 +0000

    styling navbar

--- src/app/navbar/page.tsx
+++ src/app/navbar/page.tsx
@@ -1,16 +1,25 @@
 import Link from "next/link"
+import Image from "next/image"
 
 export default function NavBar() {
     return (
     
         <>
-            <div className="d-flex flex-row">
-
-                <Link href="users/create-a-fighter" className="p-2">Create A Fighter</Link>
-                <Link href="users/view-all-fighters" className="p-2">Fighters</Link>
-                <Link href="events" className="p-2">Events</Link>
-                <Link href="users/fighter/{id}" className="p-2">Profile</Link>
-
+            <div className="d-flex justify-between items-center bg-[#1E1E24]">
+                <div className="flex">
+                    <Image
+                        src="/CombatConnectLogo.png"
+                        width={80}
+                        height={80}
+                        alt="Website Logo"
+                    />
+                </div>
+                <div className="flex items-center space-x-4">
+                    <Link href="users/create-a-fighter" className="p-2 text-xl text-[#FFCF99]">Create A Fighter</Link>
+                    <Link href="users/view-all-fighters" className="p-2 text-xl text-[#FFCF99]">Fighters</Link>
+                    <Link href="events" className="p-2 text-xl text-[#FFCF99]">Events</Link>
+                </div>
+                <Link href="users/fighter/{id}" className="p-2 text-xl text-[#FFCF99]">Profile</Link>
             </div>
     
         </>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,18 +5,6 @@ export default function Home() {
   return (
     <>
       <NavBar />
-      <h1 className="text-4xl mb-4">Dashboard</h1>
-      <div className="flex justify-between items-center mb-4">
-        <Link href="users/create-a-fighter" className="text-2xl">
-          Create a Fighter
-        </Link>
-        <Link href="users/fighter/{id}" className="text-2xl">
-          Profile
-        </Link>
-        <Link href="users/view-all-fighters" className="text-2xl">
-          View all Fighters
-        </Link>
-      </div>
     </>
   );
 }
